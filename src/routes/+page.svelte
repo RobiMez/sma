1224388b84da267d4df0a4f20cae3d51d0a67af3
0x1e-robi-ghd <robelmezemir@gmail.com>
2023-09-02 17:38:42 +0300

    added tailwind + tailwind formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	console.log();
+</script>
+
+<button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+    ...</button>
+<div class=" pt-10 p-9 pb-4 w-full h-full border border-black flex-grow flex flex-col justify-center items-center">
+<h1 class="text-4xl">Welcome</h1>
+</div>
