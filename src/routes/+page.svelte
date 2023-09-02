28fef50626af84da60b7152b1b375a8945416986
0x1e-robi-ghd <robelmezemir@gmail.com>
2023-09-02 17:38:44 +0300

    tailwind formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	console.log();
 </script>
 
-<button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
-    ...</button>
-<div class=" pt-10 p-9 pb-4 w-full h-full border border-black flex-grow flex flex-col justify-center items-center">
-<h1 class="text-4xl">Welcome</h1>
+<div
+	class="flex h-full w-full flex-grow flex-col items-center justify-center border border-black "
+>
+	<h1 class="text-4xl">Welcome</h1>
 </div>
