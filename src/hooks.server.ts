875833f404827f947cbdf13f167de35d9910fe13
Robi <robelmezemir@gmail.com>
2024-05-04 20:27:00 +0300

    feat: add sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,12 @@
-
+import {sequence} from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
 import { dbConnect } from "$lib/db";
+
+Sentry.init({
+    dsn: "https://2d59ea8dc270b68ae5300743a2adfeae@o879779.ingest.us.sentry.io/4507160073601024",
+    tracesSampleRate: 1
+})
+
 dbConnect();
+export const handleError = Sentry.handleErrorWithSentry();
+export const handle = sequence(Sentry.sentryHandle());
