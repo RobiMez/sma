16be2f3858ebff852a2efd1a82cbccc7dcee020b
Robi <robelmezemir@gmail.com>
2024-05-05 06:16:34 +0300

    chore(general): formatting some whitespace none

--- src/routes/api/pgp/+server.ts
+++ src/routes/api/pgp/+server.ts
@@ -1,61 +1,61 @@
-              import { json } from '@sveltejs/kit';
-              import Listener from '../../../models/listener.schema';
+import { json } from '@sveltejs/kit';
+import Listener from '../../../models/listener.schema';
 
-              interface Listener {
-                pbKey: string;
-                rid: string;
-              }
+interface Listener {
+  pbKey: string;
+  rid: string;
+}
 
-              export async function GET({ url }) {
-                const rid = url.searchParams.get('r') ?? '';
-                const lim = parseInt(url.searchParams.get('lim') ?? '100');
+export async function GET({ url }) {
+  const rid = url.searchParams.get('r') ?? '';
+  const lim = parseInt(url.searchParams.get('lim') ?? '100');
 
-                const user = await Listener.findOne({ rid }, { messages: { $slice: -lim } });
+  const user = await Listener.findOne({ rid }, { messages: { $slice: -lim } });
 
-                if (user) {
-                  return json({ status: 200, body: user });
-                }
+  if (user) {
+    return json({ status: 200, body: user });
+  }
 
-                return json({ status: 404, body: 'Public key not found' });
-              }
+  return json({ status: 404, body: 'Public key not found' });
+}
 
-              export async function PATCH({ request }) {
-                const body = await request.json();
-                const { message, r, p, timestamp } = body;
+export async function PATCH({ request }) {
+  const body = await request.json();
+  const { message, r, p, timestamp } = body;
 
-                try {
-                  const user = await Listener.findOneAndUpdate(
-                    { rid: p },
-                    { $push: { messages: { message, r, timestamp } } },
-                    { new: true }
-                  );
+  try {
+    const user = await Listener.findOneAndUpdate(
+      { rid: p },
+      { $push: { messages: { message, r, timestamp } } },
+      { new: true }
+    );
 
-                  if (user) {
-                    return json({ status: 200, body: user });
-                  } else {
-                    return json({ status: 404, body: 'User not found' });
-                  }
-                } catch (error) {
-                  console.error(error);
-                  return json({ status: 500, body: 'Error updating user' });
-                }
-              }
+    if (user) {
+      return json({ status: 200, body: user });
+    } else {
+      return json({ status: 404, body: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    return json({ status: 500, body: 'Error updating user' });
+  }
+}
 
-              export async function POST({ request }) {
-                const body = await request.json();
-                const { pbKey, rid } = body as unknown as Listener;
-                const newListener = new Listener({
-                  pbKey,
-                  rid,
-                  title: rid,
-                  messages: []
-                });
+export async function POST({ request }) {
+  const body = await request.json();
+  const { pbKey, rid } = body as unknown as Listener;
+  const newListener = new Listener({
+    pbKey,
+    rid,
+    title: rid,
+    messages: []
+  });
 
-                try {
-                  await newListener.save();
-                  return json({ status: 200, body: 'Listener saved successfully' });
-                } catch (error) {
-                  console.error(error);
-                  return json({ status: 500, body: 'Error saving listener' });
-                }
-              }
+  try {
+    await newListener.save();
+    return json({ status: 200, body: 'Listener saved successfully' });
+  } catch (error) {
+    console.error(error);
+    return json({ status: 500, body: 'Error saving listener' });
+  }
+}
